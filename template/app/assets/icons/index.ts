27b667f808ff031f44fbd10ed83448475fcd6c70
Mustafa Akbas <mustafa.akbas@apposing.co.uk>
2021-11-09 10:54:14 +0000

    update icons color option

--- template/app/assets/icons/index.ts
+++ template/app/assets/icons/index.ts
@@ -1,3 +1,14 @@
-import Home from './Home.svg';
+/**
+ * If inside the svg file has fill and stroke value as #000
+ * that might be provide flexibility to use dynamic color
+ * If you need more config update the .svgrrc file
+ *
+ * @format
+ * Developed by Mustafa Akbas
+ */
 
-export {Home};
+import Home from './Home.svg';
+import NoProfile from './NoProfile.svg';
+import HeartFilled from './HeartFilled.svg';
+
+export {Home, NoProfile, HeartFilled};
