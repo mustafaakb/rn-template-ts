4c390829dc3a24d63978209b6f3106775b071225
Mustafa Akbas <mustafa.akbas@apposing.co.uk>
2021-11-16 09:25:24 +0000

    babel resolver path alias

--- template/.eslintrc.js
+++ template/.eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  extends: ['plugin:import/errors', 'plugin:import/warnings'],
+  settings: {
+    'import/resolver': {
+      'babel-module': {},
+    },
+  },
 };
