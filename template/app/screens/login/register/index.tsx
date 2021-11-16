bc045b058c6341b8652bac86f112b97cbf63fe7f
Mustafa Akbas <mustafa.akbas@apposing.co.uk>
2021-11-16 14:47:26 +0000

    fix navigation

--- template/app/screens/login/register/index.tsx
+++ template/app/screens/login/register/index.tsx
@@ -1,13 +1,10 @@
 import {apiEndpoints, AxiosContext} from '@app/api';
 import {SCREENS} from '@app/screens';
-import {useNavigation} from '@react-navigation/core';
 import React, {useContext} from 'react';
 import {Button, View} from 'react-native';
 
-export const RegisterScreen = () => {
+export const RegisterScreen = ({navigation}) => {
   const {axios} = useContext(AxiosContext);
-  const {navigation} = useNavigation();
-
   const handleLogin = async () => {
     //const params: REGISTERParams = {};
     axios
